de141978d4e124f79c328fda93d791c58fb6acf8
Mary <marieshix98@gmail.com>
2022-11-29 23:14:48 +0300

    Update global background color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex px-16 shadow font-semibold bg-teal-200 w-full justify-between h-20 items-center">
+    <nav className="flex px-16 shadow font-semibold  w-full justify-between h-20 items-center">
       <h2>Brain Teaser</h2>
       <div>
         <button className=" bg-rose-500 px-10 py-2 bg-red rounded outline-none uppercase  text-center text-base text-white">
