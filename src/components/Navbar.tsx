3519802fadb97aa5fa553f4b1b7b6c216f2d7a15
Mary <marieshix98@gmail.com>
2022-11-30 17:31:41 +0300

    Add final resuls page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <nav className="flex px-16 shadow font-semibold  w-full justify-between h-20 items-center">
-      <h2>Brain Teaser</h2>
+      <h2 onClick={() => navigate("/form")}>Brain Teaser</h2>
       <div>
         <button className=" bg-rose-500 px-10 py-2 bg-red rounded outline-none uppercase  text-center text-base text-white">
           Sign up
---
+++ src/pages/FinalResults.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FinalResults = () => {
+  return <div>FinalResults</div>;
+};
+
+export default FinalResults;
